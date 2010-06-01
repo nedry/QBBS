f1d1145f8722bcaac844121867243f44dad7d66c
Martin DeMello <martindemello@gmail.com>
2010-06-01 20:13:16 +0530

    minor cleanups in menu code

--- menu.rb
+++ menu.rb
@@ -4,43 +4,36 @@ def readmenu(args)
   ptr = args[:initval] || 1
 
   range = args[:range]
-  out = nil
   out = args[:out]
   done = false
   o_prompt = args[:prompt]
   high = args[:range].last
-  while true
 
+  while true
     prmpt = o_prompt.gsub("%p","#{ptr}")
     inp = getinp(eval(prmpt))
     oldptr = ptr
     sel = inp.upcase
 
-    # Martin, can you do this better?
-    high = 0 if high.nil?
-    low = args[:range].first
-    low = 0 if low.nil?
-    ptr = 0 if ptr.nil?
-
+    high ||= 0
+    low = args[:range].first || 0
+    ptr ||= 0 if ptr.nil?
 
     case sel
-    when ""; ptr = (dir == 1) ? up(ptr, high,out) : down(ptr, low)
+    when ""; ptr = (dir == 1) ? up(ptr, high, out) : down(ptr, low)
     when "-"; dir = -1; ptr = down(ptr, low)
-    when "+"; dir = +1; ptr = up(ptr, high,out)
+    when "+"; dir = +1; ptr = up(ptr, high, out)
     when /\d+/
-      ptr = jumpto(ptr, sel.to_i, low,high) if sel.to_i != 0
-      #when /[0..9]/; ptr = jumpto(ptr, sel.to_i, low,high)
+      ptr = jumpto(ptr, sel.to_i, low, high) if sel.to_i != 0
     end 
     moved = (ptr != oldptr)
-    ptr,high = yield(sel, ptr, moved)
-    #print ptr == true
+    ptr, high = yield(sel, ptr, moved)
     break if ptr == true # exit if a -1 is returned
     sdir = (dir > 0) ? '+' : '-'
   end
 end
 
 def up(ptr, high, out)
-
   if ptr < high
     ptr = ptr + 1
   else
@@ -52,7 +45,6 @@ def up(ptr, high, out)
     end
   end
   ptr
-  #return [ptr,out]
 end
 
 def down(ptr, low)
@@ -66,9 +58,8 @@ end
 
 def jumpto(ptr, newptr, low, high)
   if (newptr >= low) and (newptr <= high) then 
-    ptr = newptr
+    newptr
   else
-    print "Im here"
     print "Out of Range."
     ptr
   end
