932089a7adcc0a8d176357b8dc39807d6bc44584
Dennis Nedry <dennis@cortex-media.info>
2010-06-12 01:07:39 +0100

    Web

--- web/webserver.rb
+++ web/webserver.rb
@@ -1,8 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require "pg_ext"
 
-require 'functions.rb'
+require "../db.rb"
+require "../db_user.rb"
+require "../db_who.rb"
+require "../consts.rb"
+
+
+ enable :sessions
+
+TEXT_ROOT = "/home/mark/qbbs/text/"
+TITLE = "QUARKseven Web Interface (v.01)"
 
 helpers do
 
@@ -22,12 +32,53 @@ helpers do
   end
 end
 
+ def text_to_html (f_name)
+
+   filename = TEXT_ROOT+f_name
+   output = ""
+
+ 	if File.exists?(filename) 
+		output << "<br>"
+ 		IO.foreach(filename) { |line| line=line+"<br>" 
+ 		  line.gsub!(" ","&nbsp;")
+ 		  output << line } 
+ 	else
+ 		output =  "<br>#{filename} has run away...please tell sysop!<br>"
+ 	end
+   return output
+ end
+
+def w_open_database
+ 
+ begin
+  @db = PGconn.connect(DATAIP,5432,"","",DATABASE,DB_UID,DB_PASSWD)
+ rescue
+  print "Fatal Error: Database Connection Failed.  Halted."
+ end
+end
+
+def close_database
+  @db.close
+end
 
 get '/' do
-  haml :index
- #"<h1>Hello world  #{Time.now}</h1>"
+	
+  haml :index, :locals => {:display_text => text_to_html("welcome1.txt")}
 end
 
 get '/about' do
-  haml :about
+  haml :about, :locals =>{:title => TITLE}
+end
+
+post '/clogon' do
+  open_database
+  name = params["acc_name"]
+  passwd = params["password"]
+  if user_exists(name) then 
+   if check_password(name,passwd) then
+     haml :success 
+   else
+     haml :failure
+  end
+end
 end
