5dd22057548c123b4b92011c3b535dedc7ac2481
Dennis Nedry <dennis@cortex-media.info>
2010-07-12 20:34:57 +0100

    huh

--- qwk.rb
+++ qwk.rb
@@ -174,12 +174,15 @@ module Qwk
         # read blocks
         file.pos = (startrec + 1) * 128
         if message.blocks > 1 then
+
           temp = file.read((message.blocks - 1) * 128)
-          message.text = convert_to_utf8(temp)
+	 # message_text = temp.force_encoding("IBM437").encode("UTF-8")
+	  message.text = convert_to_utf8(temp)
+ 
         end
       end
+     @log.write("NSREC : #{startrec + message.blocks}")
 
-      @log.write("NSREC : #{startrec + message.blocks}")
       @log.write("")
       @log.write("ERROR: Corrupt packet detected.") if message.error
 
