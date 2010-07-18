5e100d73460b1f5a75ad3ed0c7d215826b153b8c
Martin DeMello <martindemello@gmail.com>
2010-07-19 03:50:05 +0530

    remove unused functions from db.rb

--- db/db_email.rb
+++ db/db_email.rb
@@ -8,7 +8,6 @@ def new_email(ind,user)
   ind = 0 if ind.nil?
   Message.all(:number => 0, :absolute.gt => ind,  :conditions => ["m_to ILIKE ?", user] ).count
   #res = @db.exec("SELECT COUNT(*) FROM messages WHERE absolute > #{ind} and upper(m_to) = '#{user.upcase}' and number = '0'")
-  #result = single_result(res).to_i
 end
 
 
