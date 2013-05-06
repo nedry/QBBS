6dc27a6e5211d8ea4fae59c663cc136e6395d068
Mark Firestone <john@yoyodyne-propulsion.net>
2013-05-06 02:26:28 +0100

    db model for bbs list

---
+++ models/bbslist.rb
@@ -0,0 +1,42 @@
+require 'models/numbered'
+
+#We are keeping the field names to match the fields in the
+#Synchronet BBS list which this is compatable with.
+#So, some names don't make sense...
+
+class Other
+  include DataMapper::Resource
+  extend Numbered
+  storage_names[:default] = 'bbslist'
+
+  property :id, Serial
+  property :name, String, :length => 40
+  property :locked, Boolean, :default => false
+  property :number, Integer
+  property :modify_date, DateTime
+  property :born_date, DateTime
+  property :software, String, :length => 80
+  property :sysop, String, :length => 80
+  property :email, String, :length => 80
+  property :website, String, :length => 80
+  property :number, String, :length => 80
+  # meaningless.  I think it was minimum Baud Rate
+  property :minrate, String, :length => 80
+  # was maximum baud rate?  Now telnet port
+  property :maxrate, String, :length => 80
+  property :location, String, :length => 80  
+  #network and address seperated by ;
+  property :network, Text
+  #terminal types, seperated by ;
+  property :terminal, String, :length => 255 
+  property :megs, Integer
+  property :msgs, Integer
+  property :files, Integer
+  property :nodes, Integer
+  property :users, Integer  
+  property :subs, Integer
+  property :dirs, Integer
+  property :xterns, Integer
+  property :desc, Text
+  
+end
